a8131f0811e882b867478b952b5b87b8cdb7f28e
musaibjashore <ahsan.jessore.109@gmail.com>
2026-02-21 12:01:36 +0600

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9@r3arrrl3n-fppt^l-7e+huk!x2er%((r%57w-%j9&+&_w+1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -129,4 +129,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = BASE_DIR/"media"
